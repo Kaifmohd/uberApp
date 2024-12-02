f38ac5653d67bbfff7a38773c9899be88a558121
Kaif Siddique <kaifmohd75@gmail.com>
2024-12-02 22:42:08 +0100

    Added-      -Required Entities,Repository,Services.      -Added Exceptions and Advices.      -Added SignUp feature. (partial)      -Added Rider Creation and Request feature.

---
+++ src/main/java/com/sdekaif/project/uberbackend/uberApp/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.sdekaif.project.uberbackend.uberApp.exceptions;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
